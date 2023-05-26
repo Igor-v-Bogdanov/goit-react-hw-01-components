d004d83bf7f516c86e27bbe4c6f733dabc2dd156
Igor-V-Bogdanov <thehogjer@gmail.com>
2023-05-27 01:47:37 +0300

    HW #1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import user from 'components/data/user';
+import { Profile } from './Profile/Profile';
+import data from 'components/data/data';
+import { Statistics } from './Statistics/Statistics';
+import friends from 'components/data/friends';
+import { FriendList } from './FriendList/FriendList';
+import transactions from 'components/data/transactions';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      goit-react-hw-01-components
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
